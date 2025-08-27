6c4b183a031016b880b580971018a63603fb8c7c
Xinyang Li <soybeanyoung@gmail.com>
2025-08-27 03:09:25 -0400

    Match the versions in lock

--- install.sh
+++ install.sh
@@ -248,8 +248,11 @@ install_immich_web_server_pnpm () {
         pnpm config set registry=$PROXY_NPM
     fi
 
-    jq '(.pnpm.overrides += {"sharp": "0.34.2", "nestjs-kysely": "0.5.1", "kysely": "0.27.3"}) | del(.overrides)' \
+    jq '(.pnpm.overrides += {"sharp": "0.34.2", "nestjs-kysely": "3.0.0", "kysely": "0.28.2"}) | del(.overrides)' \
         server/package.json > server/package.json.tmp && mv server/package.json.tmp server/package.json
+
+    # debugging
+    cat server/package.json 
 
     rm -r $INSTALL_DIR_app 
     
